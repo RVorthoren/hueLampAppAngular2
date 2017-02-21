ef998f7149f55e229eafa127840bb1cf3ab78778
Remco <Remco.boys@gmail.com>
2017-02-21 14:30:17 +0100

    Add routes for home, config and lights. Add lights component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ConfigurationComponent } from './configuration/configuration.component';
+import { LightsComponent } from './lights/lights.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ConfigurationComponent
+    ConfigurationComponent,
+    LightsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'config',
+        component: ConfigurationComponent
+      },
+      {
+        path: 'lights',
+        component: LightsComponent
+      },
+      {
+        path: '**',
+        redirectTo: '/home'
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
